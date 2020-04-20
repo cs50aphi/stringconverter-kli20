61bbda4a7ced68dbb8e097c684741441cf8e0b7c
Kate Li <kli20@punahou.edu>
2020-04-20 00:24:10 +0000

    finished coding, time to comment

--- StringConverter.java
+++ StringConverter.java
@@ -9,12 +9,10 @@ public class StringConverter
         // getBytes() method to convert string into bytes[]
         byte [] strAsByteArray = input.getBytes();
         byte [] result = new byte [strAsByteArray.length];
-
         // store result in reverse order into the result byte[]
         for (int i = 0; i < strAsByteArray.length; i++) {
             result[i] = strAsByteArray[strAsByteArray.length - i - 1];
         }
-
         return new String(result);
     }
 
@@ -36,29 +34,22 @@ public class StringConverter
             return false;
         }
     }
+    
     public static String pigLatinate(String englishWord) {
-        int vowel = -1;
-        for (int i = 0; i < englishWord.length(); i++) {
-            // if englishWord has a vowel, return the index
-            if (englishWord.charAt(i) == 'a' || englishWord.charAt(i) == 'e' || englishWord.charAt(i) == 'i' || englishWord.charAt(i) == 'o' || englishWord.charAt(i) == 'u' || englishWord.charAt(i) == 'A' || englishWord.charAt(i) == 'E' || englishWord.charAt(i) == 'I' || englishWord.charAt(i) == 'O' || englishWord.charAt(i) == 'U') {
-                break;
-            }
-            vowel = i;
-        }
         // if englishWord has no vowels, return englishWord + "ay"
-        if (vowel < 0) {
+        if (checkVowel(englishWord) < 0) {
             return englishWord + "ay";
         }
         // else if englishWord begins with a vowel, return englishWord + "yay"
-        else if (vowel == 0) {
+        else if (checkVowel(englishWord) == 0) {
             return englishWord + "yay";
         }
         // else if englishWord has a vowel in it and yet doesn't start with a vowel, return end + start + "ay"
         else {
             // Let start be all of englishWord up to (but not including) its first vowel.
-            String start = englishWord.substring(0, vowel);
+            String start = englishWord.substring(0, checkVowel(englishWord));
             // Let end be all of englishWord from its first vowel on
-            String end = englishWord.substring(vowel);
+            String end = englishWord.substring(checkVowel(englishWord));
             // Convert string to array of characters to check capitalization
             char[] charArray = englishWord.toCharArray();
             for (int i = 0; i < charArray.length; i++) {
@@ -69,13 +60,27 @@ public class StringConverter
             return end + start + "ay";
         }
     }
-    // public static int checkVowel(String englishWord) {
-    //     for (int i = 0; i < englishWord.length(); i++) {
-    //         // if englishWord has a vowel, return the index
-    //         if ((englishWord.charAt(i) == 'a') || (englishWord.charAt(i)) == 'e') || (englishWord.charAt(i)) == 'i') || (englishWord.charAt(i)) == 'o') || (englishWord.charAt(i)) == 'u') || (englishWord.charAt(i)) == 'A') || (englishWord.charAt(i)) == 'E') || (englishWord.charAt(i)) == 'I') || (englishWord.charAt(i)) == 'O') || (englishWord.charAt(i)) == 'U')) {
-    //             return i;
-    //         }
-    //     }
-    //     return -1;
-    // }
+    
+    public static String shorthand(String input) {
+        // Replace:"and" >> "&", "to" >> "2", "you" >> "U", and "for" >> "4"
+        input = input.replaceAll(" and ", " & ");
+        input = input.replaceAll(" to ", " 2 ");
+        input = input.replaceAll(" you ", " U ");
+        input = input.replaceAll(" for ", " 4 ");
+        // replaces (deletes) all vowels
+        input = input.replaceAll("[aeiouAEIO]", "");
+        return input;
+    }
+    
+    // Checks if englishWord has vowels
+    // used in pigLatinate()
+    public static int checkVowel(String englishWord) {
+        for (int i = 0; i < englishWord.length(); i++) {
+            // if englishWord has a vowel, return the index
+            if (englishWord.charAt(i) == 'a' || englishWord.charAt(i) == 'e' || englishWord.charAt(i) == 'i' || englishWord.charAt(i) == 'o' || englishWord.charAt(i) == 'u' || englishWord.charAt(i) == 'A' || englishWord.charAt(i) == 'E' || englishWord.charAt(i) == 'I' || englishWord.charAt(i) == 'O' || englishWord.charAt(i) == 'U') {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- StringConverterDriver.java
+++ StringConverterDriver.java
@@ -38,52 +38,52 @@ public class StringConverterDriver
         System.out.println(breakUp(str)); // passes str to breakUp, which in turn calls pigLatinate (see below)
         System.out.println("-----------------------------");
 
-        //     // Shorthand
+        // Shorthand
 
-        //     String[] shorts = new String[3];
-        //     shorts[0] = "For you and me to be best friends, we must be all to each other.";
-        //     shorts[1] = "You are not as funny as you think you are.";
-        //     shorts[2] = "Using hand tools often belies competence.";
-        //     for (String s : shorts) {
-        //         System.out.println(s);
-        //         // passes str to shorthand, which returns the converted shorthand form.
-        //         System.out.println(StringConverter.shorthand(s));
-        //     }
-        //     System.out.println("-----------------------------");
-        // }
-
-        /**
-        * Recursive function that takes a sentence and breaks it into individual words,
-        * passing each word on to a StringConverter method. Then it reconstructs the sentence
-        * and returns it to the main method.
-        * @param str String that represents a sentence that will be broken up.
-        */
-        private static String breakUp(String str) {
-            //if single word->base case for recursion
-            if (str.indexOf(" ") == -1)
-            {
-                if (str.substring(str.length() - 1).equals(".")) {
-                    str = str.substring(0, str.length() - 1);
-                }
-                return StringConverter.pigLatinate(str);
-            }
-
-            //if multiple words, pull out first word, send rest through the recursive loop
-            String topig = "";
-            String punc = " ";
-            int i = str.indexOf(" ");
-            String punctuation = ",.;?!:#$%&*)";
-            if (punctuation.contains(str.substring(i - 1, i)))
-            {
-                    topig = str.substring(0, i - 1);
-                    punc = str.substring(i - 1);
-            }
-            else
-            {
-                topig = str.substring(0, i);
-            }
-            str = str.substring(i + 1,str.length());
-            return StringConverter.pigLatinate(topig) + punc + breakUp(str);
+        String[] shorts = new String[3];
+        shorts[0] = "For you and me to be best friends, we must be all to each other.";
+        shorts[1] = "You are not as funny as you think you are.";
+        shorts[2] = "Using hand tools often belies competence.";
+        for (String s : shorts) {
+            System.out.println(s);
+            // passes str to shorthand, which returns the converted shorthand form.
+            System.out.println(StringConverter.shorthand(s));
         }
+        System.out.println("-----------------------------");
+    }
+
+    /**
+    * Recursive function that takes a sentence and breaks it into individual words,
+    * passing each word on to a StringConverter method. Then it reconstructs the sentence
+    * and returns it to the main method.
+    * @param str String that represents a sentence that will be broken up.
+    */
+    private static String breakUp(String str) {
+        //if single word->base case for recursion
+        if (str.indexOf(" ") == -1)
+        {
+            if (str.substring(str.length() - 1).equals(".")) {
+                str = str.substring(0, str.length() - 1);
+            }
+            return StringConverter.pigLatinate(str);
+        }
+
+        //if multiple words, pull out first word, send rest through the recursive loop
+        String topig = "";
+        String punc = " ";
+        int i = str.indexOf(" ");
+        String punctuation = ",.;?!:#$%&*)";
+        if (punctuation.contains(str.substring(i - 1, i)))
+        {
+                topig = str.substring(0, i - 1);
+                punc = str.substring(i - 1);
+        }
+        else
+        {
+            topig = str.substring(0, i);
+        }
+        str = str.substring(i + 1,str.length());
+        return StringConverter.pigLatinate(topig) + punc + breakUp(str);
     }
 }
+
